c41281c65e0ff2a0ee7bff1ea27d0a5398ccdbd7
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2024-12-17 20:22:25 +0530

    CrossOrgin is added for book controller

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +21,13 @@ import com.example.demo.dto.response.BookResponseDTO;
 import com.example.demo.service.BookService;
 
 @RestController
+/*
+ * It allows you to specify which external origins (i.e., domains or URLs) are
+ * permitted to make requests to your API. This is useful when your frontend
+ * application (running on a different server or port) needs to interact with
+ * your backend
+ */
+@CrossOrigin(origins = "*")
 public class BookController {
 
     @Autowired
